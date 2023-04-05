3d5e9b0cb8ff44a896dd629f9483361215d74ad5
Julia Lipniewicz <julialipniewicz@yahoo.de>
2023-04-05 15:31:33 +0200

    getting started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import React from "react";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gardient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+};
+
 function App() {
-  return <div className="App"></div>;
+  return <div className={style.bg}></div>;
 }
 
 export default App;
